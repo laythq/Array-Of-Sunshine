bf081447e03cfcebd9f1fb05bcfe7c4e89f914fd
Bibi Collins <bibicollins94@googlemail.com>
2018-08-22 17:21:45 +0100

    FEATURE Add functionality for .toString
    Refactor findMethod to return an array allowing the return of multiple methods

--- src/model.js
+++ src/model.js
@@ -3,16 +3,22 @@ const methodList = [
   Array.prototype.pop,
   Array.prototype.reverse,
   Array.prototype.shift,
+  Array.prototype.slice,
+  Array.prototype.toString,
 ];
 
 module.exports = {
   findMethod(inputArray, desiredOutput) {
+    let outputArray = []
     for (const i in methodList) {
       const testArray = inputArray.slice()
       if (JSON.stringify(methodList[i].call(testArray)) === JSON.stringify(desiredOutput)) {
-        return `.${methodList[i].name}`;
+        outputArray.push(`.${methodList[i].name}`)
       }
     }
-    return 'No method found';
+    if (outputArray.length > 0) {
+       return outputArray
+    }
+    return ['No method found'];
   },
 };
--- src/model.test.js
+++ src/model.test.js
@@ -1,21 +1,29 @@
 import { findMethod } from './model';
 
 test('it returns .join when required for desiredOutput', () => {
-  expect(findMethod([1,2,3], "1,2,3" )).toBe(".join");
+  expect(findMethod([1,2,3], "1,2,3" )).toContain(".join");
 });
 
 test("it returns 'no method found' if it is unable to find a method", () => {
-  expect(findMethod([1,2,3],[2342,534534,65464654])).toBe('No method found');
+  expect(findMethod([1,2,3],[2342,534534,65464654])).toContain('No method found');
 });
 
 test('it returns .pop when appropriate', () => {
-  expect(findMethod([1,2,3], 3)).toBe('.pop')
+  expect(findMethod([1,2,3], 3)).toContain('.pop')
 })
 
 test('it returns .reverse when appropriate', () => {
-  expect(findMethod([1,2,3], [3,2,1])).toBe('.reverse')
+  expect(findMethod([1,2,3], [3,2,1])).toContain('.reverse')
 })
 
 test('it returns .shift when appropriate', () => {
-  expect(findMethod([1,2,3], 1)).toBe('.shift')
+  expect(findMethod([1,2,3], 1)).toContain('.shift')
+})
+
+test('it returns .slice when appropriate', () => {
+  expect(findMethod([1,2,3], [1,2,3])).toContain('.slice')
+})
+
+test('it returns .toString when appropriate', () => {
+  expect(findMethod([1,2,3], "1,2,3")).toContain('.toString')
 })
