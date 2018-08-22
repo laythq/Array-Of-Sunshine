09227c5ce155d73346402097b9b054338b206eb9
Elishka Flint <elishkaf@gmail.com>
2018-08-22 18:26:56 +0100

    VIEW Add basic React functionality to take input / output and render a code suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {change} from './placeholder_model_EF';
 
-class App extends Component {
+// playing with creating a history, ignore for now
+class History extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>Hello {this.props.name}</div>
+    )
+  }
+}
+// -----------------------------------------------
+
+function CodeSuggestion(props) {
+  return (
+    <div>{props.input} > {props.output} = {props.code}</div>
+  )
+}
+
+class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: 'input',
+      output: 'output',
+      code: 'code'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderCodeSuggestion = this.renderCodeSuggestion.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    const code = change(this.state.input, this.state.output)
+    this.setState({
+      code: code
+    })
+    event.preventDefault();
+  }
+
+  renderCodeSuggestion() {
+    return (
+      <CodeSuggestion
+        input={this.state.input}
+        output={this.state.output}
+        code={this.state.code}
+      />
+    )
+  }
+
+  // playing with creating a history, ignore for now
+  renderHistory(){
+    return (
+      <History name="elishka" />
+    )
+  }
+  // -----------------------------------------------
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload. this is a line that is way too long
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input name="input" type="text" onChange={this.handleChange} />
+          <input name="output" type="text" onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        {this.renderCodeSuggestion()}
+        {/* playing with creating a history, ignore for now */}
+        {this.renderHistory()}
       </div>
     );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <InputForm />
+      </div>
+    )
   }
 }
 
