d0a2a8dccf437e40ef7f17105cfe54045dbeff9d
John Forster <j_forster@hotmail.co.uk>
2018-08-22 17:59:34 +0100

    FEATURE add workOutType method to parse strings

--- src/model.js
+++ src/model.js
@@ -9,16 +9,20 @@ const methodList = [
 
 module.exports = {
   findMethod(inputArray, desiredOutput) {
-    let outputArray = []
+    const outputArray = [];
     for (const i in methodList) {
-      const testArray = inputArray.slice()
+      const testArray = inputArray.slice();
       if (JSON.stringify(methodList[i].call(testArray)) === JSON.stringify(desiredOutput)) {
-        outputArray.push(`.${methodList[i].name}`)
+        outputArray.push(`.${methodList[i].name}`);
       }
     }
     if (outputArray.length > 0) {
-       return outputArray
+      return outputArray;
     }
     return ['No method found'];
+  },
+
+  workOutType(string) {
+    if (string.match(/^'.*'$/)) return string.replace(/[(^')('$)]/g, '');
   },
 };
--- src/model.test.js
+++ src/model.test.js
@@ -1,29 +1,33 @@
-import { findMethod } from './model';
+import { findMethod, workOutType } from './model';
 
 test('it returns .join when required for desiredOutput', () => {
-  expect(findMethod([1,2,3], "1,2,3" )).toContain(".join");
+  expect(findMethod([1, 2, 3], '1,2,3')).toContain('.join');
 });
 
 test("it returns 'no method found' if it is unable to find a method", () => {
-  expect(findMethod([1,2,3],[2342,534534,65464654])).toContain('No method found');
+  expect(findMethod([1, 2, 3], [2342, 534534, 65464654])).toContain('No method found');
 });
 
 test('it returns .pop when appropriate', () => {
-  expect(findMethod([1,2,3], 3)).toContain('.pop')
-})
+  expect(findMethod([1, 2, 3], 3)).toContain('.pop');
+});
 
 test('it returns .reverse when appropriate', () => {
-  expect(findMethod([1,2,3], [3,2,1])).toContain('.reverse')
-})
+  expect(findMethod([1, 2, 3], [3, 2, 1])).toContain('.reverse');
+});
 
 test('it returns .shift when appropriate', () => {
-  expect(findMethod([1,2,3], 1)).toContain('.shift')
-})
+  expect(findMethod([1, 2, 3], 1)).toContain('.shift');
+});
 
 test('it returns .slice when appropriate', () => {
-  expect(findMethod([1,2,3], [1,2,3])).toContain('.slice')
-})
+  expect(findMethod([1, 2, 3], [1, 2, 3])).toContain('.slice');
+});
 
 test('it returns .toString when appropriate', () => {
-  expect(findMethod([1,2,3], "1,2,3")).toContain('.toString')
-})
+  expect(findMethod([1, 2, 3], '1,2,3')).toContain('.toString');
+});
+
+test('workOutType turns a single quoted string into a string', () => {
+  expect(workOutType("'This is a string'")).toBe('This is a string');
+});
