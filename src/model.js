27a1378da6e19656a2b54d555a16f7b03f6ad268
John Forster <j_forster@hotmail.co.uk>
2018-08-22 16:14:59 +0100

    REFACTOR add dummy to model.js to prevent errors
    Also various changes to satisfy ESLint.

--- src/model.js
+++ src/model.js
@@ -1,14 +1,15 @@
-var arrayList = [
-  Array.prototype.join
-]
+const methodList = [
+  Array.prototype.join,
+];
 
 module.exports = {
-  findMethod: function(inputArray, desiredOutput){
-    for (let i in arrayList) {
-      if (arrayList[i].call(inputArray) === desiredOutput){
-        return '.' + arrayList[i].name;
+  findMethod(inputArray, desiredOutput) {
+    for (const i in methodList) {
+      const testArray = inputArray.slice();
+      if (methodList[i].call(testArray) === desiredOutput) {
+        return `.${methodList[i].name}`;
       }
-      return 'No method found';
     }
+    return 'No method found';
   },
 };
