b04889ce0a2b977f7d6da8c9b82e7126235da50d
Bibi Collins <bibicollins94@googlemail.com>
2018-08-23 11:22:47 +0100

    TEST Add workOutArray test for when input is not an array
    REFACTOR Add describe blocks to test suite

--- src/model.test.js
+++ src/model.test.js
@@ -1,49 +1,58 @@
 import { findMethod, workOutType, workOutArray } from './model';
 
-test('it returns .join when required for desiredOutput', () => {
-  expect(findMethod([1, 2, 3], '1,2,3')).toContain('.join');
-});
+describe('workOutType', () => {
+  test('it turns a single quoted string into a string', () => {
+    expect(workOutType("'This is a string'")).toBe('This is a string');
+  });
 
-test("it returns 'no method found' if it is unable to find a method", () => {
-  expect(findMethod([1, 2, 3], [2342, 534534, 65464654])).toContain('No method found');
-});
+  test('it turns a double quoted string into a string', () => {
+    expect(workOutType('"This is a string"')).toBe('This is a string');
+  });
 
-test('it returns .pop when appropriate', () => {
-  expect(findMethod([1, 2, 3], 3)).toContain('.pop');
-});
+  test('it turns an integer string into an integer', () => {
+    expect(workOutType('2')).toBe(2);
+  });
+})
 
-test('it returns .reverse when appropriate', () => {
-  expect(findMethod([1, 2, 3], [3, 2, 1])).toContain('.reverse');
-});
+describe('workOutArray', () => {
+  test('it fully parses an array string of integers', () => {
+    expect(JSON.stringify(workOutArray('[1,2,3]'))).toBe(JSON.stringify([1, 2, 3]));
+  });
 
-test('it returns .shift when appropriate', () => {
-  expect(findMethod([1, 2, 3], 1)).toContain('.shift');
-});
+  test('it fully parses a double quoted array string of integers', () => {
+    expect(JSON.stringify(workOutArray('"[1,2,3]"'))).toBe(JSON.stringify([1, 2, 3]));
+  });
+  test('it solely works out the type of the input if not an array', () => {
+    expect(workOutArray("3")).toBe(3);
+  });
+})
 
-test('it returns .slice when appropriate', () => {
-  expect(findMethod([1, 2, 3], [1, 2, 3])).toContain('.slice');
-});
+describe('findMethod', () => {
+  test('it returns .join when required for desiredOutput', () => {
+    expect(findMethod([1, 2, 3], '1,2,3')).toContain('.join');
+  });
 
-test('it returns .toString when appropriate', () => {
-  expect(findMethod([1, 2, 3], '1,2,3')).toContain('.toString');
-});
+  test("it returns 'no method found' if it is unable to find a method", () => {
+    expect(findMethod([1, 2, 3], [2342, 534534, 65464654])).toContain('No method found');
+  });
 
-test('workOutType turns a single quoted string into a string', () => {
-  expect(workOutType("'This is a string'")).toBe('This is a string');
-});
+  test('it returns .pop when appropriate', () => {
+    expect(findMethod([1, 2, 3], 3)).toContain('.pop');
+  });
 
-test('workOutType turns a double quoted string into a string', () => {
-  expect(workOutType('"This is a string"')).toBe('This is a string');
-});
+  test('it returns .reverse when appropriate', () => {
+    expect(findMethod([1, 2, 3], [3, 2, 1])).toContain('.reverse');
+  });
 
-test('workOutType turns an integer string into an integer', () => {
-  expect(workOutType('2')).toBe(2);
-});
+  test('it returns .shift when appropriate', () => {
+    expect(findMethod([1, 2, 3], 1)).toContain('.shift');
+  });
 
-test('workOutArray fully parses an array string of integers', () => {
-  expect(JSON.stringify(workOutArray('[1,2,3]'))).toBe(JSON.stringify([1, 2, 3]));
-});
+  test('it returns .slice when appropriate', () => {
+    expect(findMethod([1, 2, 3], [1, 2, 3])).toContain('.slice');
+  });
 
-test('workOutArray fully parses a double quoted array string of integers', () => {
-  expect(JSON.stringify(workOutArray('"[1,2,3]"'))).toBe(JSON.stringify([1, 2, 3]));
-});
+  test('it returns .toString when appropriate', () => {
+    expect(findMethod([1, 2, 3], '1,2,3')).toContain('.toString');
+  });
+})
