c9b2e38b15314e35e33899f6b6a7dddfc6cae4dd
Elishka Flint <elishkaf@gmail.com>
2018-08-27 20:06:36 +0100

    REFACTOR Extract language_selector to a separate script

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const findMethod = require('./src/script.js').findMethod;
+const returnMethods = require('./src/language_selector.js').returnMethods;
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '/client/public')))
@@ -22,18 +22,7 @@ app.post('/api/scripts', function (req, res) {
   const input = req.body.input
   const output = req.body.output
   const language = req.body.language
-  const exec = require('child_process').exec
-  if(language === 'ruby'){
-    exec(`ruby ./server/src/script.rb ${input} ${output}`, function (error, stdout, stderr) {
-      return res.send(stdout)
-    })
-  } else if (language === 'python'){
-    exec(`python ./server/src/script.py ${input} ${output}`, function (error, stdout, stderr) {
-      return res.send(stdout)
-    })
-  } else if (language === 'javascript'){
-    return res.send(findMethod(input, output))
-  }
+  returnMethods(language, input, output, res)
 });
 
 app.listen(process.env.PORT || 8080, () => {
