294ab55197e63c6dd1af41c00a4b7f2cf18e3921
Bibi Collins <bibicollins94@googlemail.com>
2018-08-28 16:02:42 +0100

    OTHER Heroku serverclient

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.post('/api/scripts', function (req, res) {
   returnMethods(language, input, output, res)
 });
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/../client/public/index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('CORS-enabled web server listening on port 8080 | HELLO SALLY!')
