616d2303c18ab1ee466919d7498bd3f90e22a291
Layth <laythq@gmail.com>
2018-08-28 18:16:59 +0100

    Rewind find_method changes

--- server/src/language_selector.js
+++ server/src/language_selector.js
@@ -1,11 +1,13 @@
 const findMethod = require('./script.js').findMethod;
 const exec = require('child_process').exec
+const argumentSuggestor = require('./argumentSuggestor.js')
 
 module.exports = {
   returnMethods: function(language, input, output, res) {
+    const arguments =  argumentSuggestor.suggestArguments(input, output)
     switch(language) {
       case 'ruby':
-        runRuby(input, output, res)
+        runRuby(input, output, arguments,res)
         break;
       case 'python':
         runPython(input, output, res)
@@ -27,6 +29,7 @@ function runRuby(input, output, res) {
 
 function runPython(input, output, res) {
   exec(`python ./server/src/script.py ${input} ${output}`, function (error, stdout, stderr) {
+    argumentSuggestor.suggestArguments(input, output)
     return res.send(stdout)
   })
 }
--- server/src/script.rb
+++ server/src/script.rb
@@ -1,7 +1,89 @@
-def find_method(input, output)
-  puts "THIS IS RUBY: #{input} #{output} .reverse"
-end
+# puts 'runni'
 
 input = ARGV[0]
 output = ARGV[1]
+
+METHOD_LIST = %i[
+  clear
+  compact
+  count
+  empty?
+  first
+  flatten
+  frozen?
+  inspect
+  join
+  last
+  length
+  permutation
+  pop
+  product
+  reverse
+  rotate
+  shift
+  size
+  sort
+  to_s
+  uniq
+  values_at
+].freeze
+
+# permutation
+
+def compare_arrays(array1, array2, method, arg)
+  begin
+  if arg
+    if array1.send(method, arg) == array2
+      return true
+    else
+      return false
+    end
+  else
+    if array1.send(method) == array2
+      return true
+    else
+      return false
+    end
+  end
+    rescue StandardError
+end
+end
+
+def find_method(input, output, arg = nil)
+  solution = []
+  METHOD_LIST.each do |method|
+  dummy_input = input.clone
+    if compare_arrays(dummy_input, output, method, arg)
+      solution << method
+    end
+end
+  @input = input
+  if solution.any?
+    if solution.length == 1
+      if arg
+        if arg.class == String
+          return "#{input}." + solution.pop.to_s + "('#{arg}')"
+        else
+          return "#{input}." + solution.pop.to_s + "(#{arg})"
+        end
+      else
+        puts 'reverse'
+        return "#{input}." + solution.pop.to_s
+      end
+    else
+      if arg
+        if arg.class == String
+          return solution.map {|x| "#{input}.#{x}('#{arg}')"}.join(', ')
+        else
+          return solution.map {|x| "#{input}.#{x}(#{arg})"}.join(', ')
+        end
+      else
+        return solution.map {|x| "#{input}.#{x}#{arg}"}.join(', ')
+      end
+    end
+  else
+    return 'No method found'
+  end
+end
+
 find_method(input, output)
