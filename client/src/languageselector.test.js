152aeb47a7f9e5e856531da0b6d7d42c58242db2
Elishka Flint <elishkaf@gmail.com>
2018-08-29 13:07:59 +0100

    REFACTOR Add before each blocks to Language Selector tests

--- client/src/languageselector.test.js
+++ client/src/languageselector.test.js
@@ -4,23 +4,24 @@ import { shallow } from 'enzyme'
 
 describe('language selector component', () => {
 
+  let mockSelectLanguage;
+  let wrapper;
+  beforeEach( () => {
+    mockSelectLanguage = jest.fn();
+    wrapper = shallow(<LanguageSelector selectLanguage={mockSelectLanguage} />);
+  })
+
   it('selects a language when button 1 is clicked', () => {
-    const mockSelectLanguage = jest.fn();
-    const wrapper = shallow(<LanguageSelector selectLanguage={mockSelectLanguage} />);
     wrapper.find('button').at(0).simulate('click')
     expect(mockSelectLanguage).toHaveBeenCalled();
   })
 
   it('selects a language when button 2 is clicked', () => {
-    const mockSelectLanguage = jest.fn();
-    const wrapper = shallow(<LanguageSelector selectLanguage={mockSelectLanguage} />);
     wrapper.find('button').at(1).simulate('click')
     expect(mockSelectLanguage).toHaveBeenCalled();
   })
 
   it('selects a language when button 3 is clicked', () => {
-    const mockSelectLanguage = jest.fn();
-    const wrapper = shallow(<LanguageSelector selectLanguage={mockSelectLanguage} />);
     wrapper.find('button').at(2).simulate('click')
     expect(mockSelectLanguage).toHaveBeenCalled();
   })
