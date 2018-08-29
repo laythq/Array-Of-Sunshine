3f197dc0e1773356372c735e62d55c4b0c7a8d40
Bibi Collins <bibicollins94@googlemail.com>
2018-08-29 11:13:21 +0100

    OTHER change app.use in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const returnMethods = require('./server/src/language_selector.js').returnMethods;
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname + 'build')));
+app.use('/static', express.static(path.join(__dirname + 'public')));
 app.use(cors())
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
