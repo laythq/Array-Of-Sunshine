005b2cdde6f2247679df4f1a8cf0cdfcad3b3b46
Elishka Flint <elishkaf@gmail.com>
2018-08-29 13:04:38 +0100

    REFACTOR Add before each blocks to Summary component tests

--- client/src/summary.test.js
+++ client/src/summary.test.js
@@ -2,38 +2,51 @@ import React from 'react'
 import { Summary } from './summary'
 import { shallow, mount } from 'enzyme'
 
-describe('setInputOutput', () => {
-  it('changes the input and output states', () => {
-    const wrapper = shallow(<Summary />)
-    const inst = wrapper.instance();
-    inst.setInputOutput('input', 'output')
-    expect(inst.state.input).toEqual('input')
-  })
-})
+describe('Summary component rendering', () => {
 
-describe('setLanguage', () => {
-  it('changes the language state', () => {
-    const wrapper = shallow(<Summary />)
-    const inst = wrapper.instance();
-    inst.setLanguage('language')
-    expect(inst.state.language).toEqual('language')
+  let wrapper;
+  let inst;
+  beforeEach( () => {
+    wrapper = shallow(<Summary />)
+    inst = wrapper.instance()
   })
-})
 
-describe('setSuggestion', () => {
-  it('updates the language state', () => {
-    const wrapper = shallow(<Summary />)
-    const inst = wrapper.instance();
-    inst.setSuggestion('suggestion')
-    expect(inst.state.suggestion).toEqual('suggestion')
+  describe('Rendering', () => {
+    it('renders the Input Form', () => {
+      expect(wrapper.find('InputForm').exists()).toBeTruthy()
+    })
+    it('renders the Code Suggestion', () => {
+      expect(wrapper.find('CodeSuggestion').exists()).toBeTruthy()
+    })
+    it('renders the History', () => {
+      expect(wrapper.find('History').exists()).toBeTruthy()
+    })
   })
-})
 
-describe('log suggestion', () => {
-  it('logs a suggestion in the state', () => {
-    const wrapper = shallow(<Summary />)
-    const inst = wrapper.instance();
-    inst.logSuggestion('language', 'input', 'output', 'code')
-    expect(inst.state.history).toContain('language: input > output = code')
+  describe('Functions', () => {
+    describe('setInputOutput', () => {
+      it('changes the input and output states', () => {
+        inst.setInputOutput('input', 'output')
+        expect(inst.state.input).toEqual('input')
+      })
+    })
+    describe('setLanguage', () => {
+      it('changes the language state', () => {
+        inst.setLanguage('language')
+        expect(inst.state.language).toEqual('language')
+      })
+    })
+    describe('setSuggestion', () => {
+      it('updates the language state', () => {
+        inst.setSuggestion('suggestion')
+        expect(inst.state.suggestion).toEqual('suggestion')
+      })
+    })
+    describe('log suggestion', () => {
+      it('logs a suggestion in the state', () => {
+        inst.logSuggestion('language', 'input', 'output', 'code')
+        expect(inst.state.history).toContain('language: input > output = code')
+      })
+    })
   })
 })
