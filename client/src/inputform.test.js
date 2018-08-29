c4e1a346f202e8c99a3800fcc3556ef75e423ca3
Elishka Flint <elishkaf@gmail.com>
2018-08-29 13:11:12 +0100

    REFACTOR Add before/each block to Input Form component tests

--- client/src/inputform.test.js
+++ client/src/inputform.test.js
@@ -4,37 +4,41 @@ import { shallow, mount } from 'enzyme'
 
 describe('input form component', () => {
 
-  describe('array input', () => {
+  describe('array inputs and outputs', () => {
+
+    let wrapper;
+    beforeEach( () => {
+      wrapper = shallow(<InputForm />)
+    })
+
     it('should respond to change event and change state of input form component', () => {
-      const wrapper = shallow(<InputForm />)
       wrapper.find('#input').simulate('change', {target: {name: 'input', value: 'some_input'}})
       expect(wrapper.state('input')).toEqual('some_input')
     })
-  })
-
-  describe('array output', () => {
     it('should respond to change event and change state of input form component', () => {
-      const wrapper = shallow(<InputForm />)
       wrapper.find('#output').simulate('change', {target: {name: 'output', value: 'some_output'}})
       expect(wrapper.state('output')).toEqual('some_output')
     })
   })
 
   describe('language selector child', () => {
+
+    let wrapper;
+    beforeEach( () => {
+      wrapper = mount(<InputForm />)
+    })
+
     it('sets its language to Javascript if selected in language selector child', () => {
-      const wrapper = mount(<InputForm />)
       const javascriptButton = wrapper.find('button').at(0)
       javascriptButton.find('button').at(0).simulate('click')
       expect(wrapper.state().language).toEqual('javascript')
     })
     it('sets its language to Ruby if selected in language selector child', () => {
-      const wrapper = mount(<InputForm />)
       const rubyButton = wrapper.find('button').at(1)
       rubyButton.find('button').at(0).simulate('click')
       expect(wrapper.state().language).toEqual('ruby')
     })
     it('sets its language to Python if selected in language selector child', () => {
-      const wrapper = mount(<InputForm />)
       const pythonButton = wrapper.find('button').at(2)
       pythonButton.find('button').at(0).simulate('click')
       expect(wrapper.state().language).toEqual('python')
