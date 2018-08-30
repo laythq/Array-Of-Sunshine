106ee736f77f1557ae4045489a9a99902df73284
John Forster <j_forster@hotmail.co.uk>
2018-08-30 11:56:14 +0100

    FEATURE add functionality to chain ruby methods

--- server/src/ruby/lib/ruby_model.rb
+++ server/src/ruby/lib/ruby_model.rb
@@ -23,25 +23,42 @@ METHOD_LIST = %i[
   values_at
 ].freeze
 
-def compare_arrays(array1, array2, method, arg)
-  arg ? array1.send(method, arg) == array2 : array1.send(method) == array2
+def compare_arrays(array1, array2, method, tried_arrays)
+  result = array1.send(method)
+  return true if result == array2 || array1 == array2
+  tried_arrays << [method, result]
+  return false
 rescue StandardError
 end
 
-def generateReturnString(solutions, input, arg)
-  arg = "'#{arg}'" if arg&.is_a?(String)
-  solutions.map! {|method|
-    suffix = arg ? "(#{arg})" : ''
-    "#{input}.#{method.to_s}" + suffix
-  }
+def generate_return_string(solutions, input)
+  # # arg = "'#{arg}'" if arg&.is_a?(String)
+  # solutions.map! {|method|
+  #   # suffix = arg ? "(#{arg})" : ''
+  #   "#{input}.#{method.to_s}" #+ suffix
+  # }
   solutions.any? ? solutions.join(', ') : 'No method found'
 end
 
-def find_method(input, output, arg = nil)
-  solutions = []
+def try_all_methods(input, output, solutions, tried_arrays = [], prefix = '')
+  return unless input.kind_of?(Array)
   METHOD_LIST.each do |method|
     dummy_input = input.clone
-    solutions << method if compare_arrays(dummy_input, output, method, arg)
+    solutions << "#{prefix}.#{method}" if compare_arrays(dummy_input, output, method, tried_arrays)
   end
-  generateReturnString(solutions, input, arg)
+end
+
+def find_method(input, output)
+  solutions = []
+  tried_methods = []
+  try_all_methods(input, output, solutions, tried_methods)
+
+  if solutions.empty?
+    tried_methods.each do |method, outcome|
+      prefix = ".#{method}"
+      try_all_methods(outcome, output, solutions, [], prefix)
+    end
+  end
+  # generate_return_string(solutions, input)
+  solutions.empty? ? ['No method found'] : solutions
 end
--- server/src/ruby/spec/ruby_model_spec.rb
+++ server/src/ruby/spec/ruby_model_spec.rb
@@ -1,32 +1,41 @@
 require 'ruby_model'
 
-describe 'find_method with one argument' do
-  it 'returns .count(argument) when appropriate' do
-    expect(find_method([1, 1, 1], 3, 1)).to eq('[1, 1, 1].count(1)')
-  end
-  it 'returns .first(argument), .shift(argument) when appropriate' do
-    expect(find_method(['a', 'b', 'a'], ['a', 'b'], 2)).to eq("[\"a\", \"b\", \"a\"].first(2), [\"a\", \"b\", \"a\"].shift(2)")
-  end
-  it 'returns .flatten(argument) when appropriate' do
-    expect(find_method(['a', ['b', [1, 1]]], ['a', 'b', [1, 1]], 1)).to eq("[\"a\", [\"b\", [1, 1]]].flatten(1)")
-  end
-  it 'returns .join(argument) when appropriate' do
-    expect(find_method(['a', 'b'], 'a, b', ', ')).to eq("[\"a\", \"b\"].join(', ')")
-  end
-  it 'returns .last(argument) and .pop(argument) when appropriate' do
-    expect(find_method(['a', 'b', 'c', 'd'], ['c', 'd'], 2)).to eq("[\"a\", \"b\", \"c\", \"d\"].last(2), [\"a\", \"b\", \"c\", \"d\"].pop(2)")
-  end
-  # it 'returns .product(argument) when appropriate' do
-  #   expect(find_method([1, 2, 3], [[1,4],[1,5],[2,4],[2,5],[3,4],[3,5]], [4, 5])).to eq("[1, 2, 3].product([4, 5])")
+# describe 'find_method with one argument' do
+  # it 'returns .count(argument) when appropriate' do
+  #   expect(find_method([1, 1, 1], 3, 1)).to eq('[1, 1, 1].count(1)')
   # end
-  it 'returns .rotate(argument) when appropriate' do
+  # it 'returns .first(argument), .shift(argument) when appropriate' do
+  #   expect(find_method(['a', 'b', 'a'], ['a', 'b'], 2)).to eq("[\"a\", \"b\", \"a\"].first(2), [\"a\", \"b\", \"a\"].shift(2)")
+  # end
+  # it 'returns .flatten(argument) when appropriate' do
+  #   expect(find_method(['a', ['b', [1, 1]]], ['a', 'b', [1, 1]], 1)).to eq("[\"a\", [\"b\", [1, 1]]].flatten(1)")
+  # end
+  # it 'returns .join(argument) when appropriate' do
+  #   expect(find_method(['a', 'b'], 'a, b', ', ')).to include(".join(', ')")
+  # end
+#   it 'returns .last and .pop(argument) when appropriate' do
+#     expect(find_method(['a', 'b', 'c', 'd'], ['c', 'd'], 2)).to include(".last(2)", ".pop(2)")
+#   end
+#   # it 'returns .product(argument) when appropriate' do
+#   #   expect(find_method([1, 2, 3], [[1,4],[1,5],[2,4],[2,5],[3,4],[3,5]], [4, 5])).to eq("[1, 2, 3].product([4, 5])")
+#   # end
+#   it 'returns .rotate(argument) when appropriate' do
+#
+#     expect(find_method([1, 2, 3], [3, 1, 2], 2)).to eq(".rotate(2)")
+#   end
+#   it 'returns .values_at(argument) when appropriate' do
+#     expect(find_method(['a', 'b', 'c', 'd', 'e', 'f'], ['d'], 3)).to eq(".values_at(3)")
+#   end
+# end
 
-    expect(find_method([1, 2, 3], [3, 1, 2], 2)).to eq("[1, 2, 3].rotate(2)")
-  end
-  it 'returns .values_at(argument) when appropriate' do
-    expect(find_method(['a', 'b', 'c', 'd', 'e', 'f'], ['d'], 3)).to eq("[\"a\", \"b\", \"c\", \"d\", \"e\", \"f\"].values_at(3)")
+describe 'find chained methods' do
+  it 'returns .uniq.sort when appropriate' do
+    expect(find_method([1,6,5,6,9,9,1,3], [1,3,5,6,9])).to include('.uniq.sort')
   end
 
+  it 'returns .reverse.uniq when appropriate' do
+    expect(find_method([5,5,4,3,2,2,1], [1,2,3,4,5])).to include('.reverse.uniq')
+  end
 end
 
 # describe 'compare_arrays' do
