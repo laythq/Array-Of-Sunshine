087c9edd015bdd6660c6d2f88d69f551d4b35c26
John Forster <j_forster@hotmail.co.uk>
2018-08-30 15:40:10 +0100

    FEATURE add suggested arguments to ruby model

--- server/src/language_selector.js
+++ server/src/language_selector.js
@@ -3,7 +3,7 @@ const findMethod = require('./script.js').findMethod;
 const argumentSuggestor = require('./argumentSuggestor.js');
 
 function runRuby(input, output, args, rubyRes) {
-  exec(`ruby ./server/src/script.rb ${JSON.stringify(input)} ${JSON.stringify(output)}`, function (error, stdout, stderr) {
+  exec(`ruby ./server/src/script.rb ${JSON.stringify(input)} ${JSON.stringify(output)} ${JSON.stringify(args)}`, function (error, stdout, stderr) {
     return rubyRes.send(JSON.parse(stdout));
   });
 }
