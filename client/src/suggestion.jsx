a90d91e6bcdc44aad4b62e268ebc88f33a0d4efe
Elishka Flint <elishkaf@gmail.com>
2018-08-30 14:38:34 +0100

    STYLING Add suggestions div

--- client/src/suggestion.jsx
+++ client/src/suggestion.jsx
@@ -6,9 +6,28 @@ export function CodeSuggestion(props) {
       return null;
     }
 
+
+
+    function formatSuggestion(array) {
+      let suggestions = []
+      if(Array.isArray(array)){ array.forEach( suggestion => suggestions.push(<div class="suggestion">{suggestion}</div> ))}
+      return suggestions
+    }
+
     return (
-      <div>
-        {props.language}: {props.input} > {props.output} = {props.suggestion}
+      <div id="code-suggestion">
+        <div id="language">
+          {props.language}
+        </div>
+        <div id="input">
+          {props.input}
+        </div>
+        <div id="output">
+          {props.output}
+        </div>
+        <div id="suggestions">
+          {formatSuggestion(props.suggestion)}
+        </div>
       </div>
     );
 }
