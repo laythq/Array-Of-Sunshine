f314b7c01a3f85a3c5fcd69522c2ef25a0dc49b3
Elishka Flint <elishkaf@gmail.com>
2018-08-31 09:17:48 +0100

    STYLING Fix history to show more than one search item

--- client/src/history.jsx
+++ client/src/history.jsx
@@ -6,21 +6,19 @@ export function History(props) {
   if (props.history.length === 0) {
     return null;
   }
-  //
-  // function generateHistory() {
-  //   return props.history.reverse();
-  // }
-  //
-  // const list = generateHistory();
 
-  console.log(props.history.reverse())
+  function generateHistory() {
+    return props.history.reverse().map((item, index) => <div key={index}>{item}</div>);
+  }
+
+  const list = generateHistory();
 
   return (
     <div id="history-section">
       <div id="title">
         Your Search History
       </div>
-      {props.history}
+      {list}
     </div>
   );
 }
