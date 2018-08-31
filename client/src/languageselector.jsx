96ecc5d8ee5a15aaadfc422b99393721fc36d915
Elishka Flint <elishkaf@gmail.com>
2018-08-31 08:16:02 +0100

    STYLING Change buttons

--- client/src/languageselector.jsx
+++ client/src/languageselector.jsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import css from './languageselector.css';
 
-export function LanguageSelector(props) {
-
-  function handleClick(language) {
-    props.selectLanguage(language)
+export class LanguageSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      javascript: 'dead',
+      ruby: 'dead',
+      python: 'dead',
+    };
   }
 
-  return (
-    <div id="language-selector">
-      <div id="buttons" align="center">
-        <button id="language" onClick={() => handleClick('javascript')}>
-          Javascript
-        </button>
-        <button id="language" onClick={() => handleClick('ruby')}>
-          Ruby
-        </button>
-        <button id="language" onClick={() => handleClick('python')}>
-          Python
-        </button>
+  setButtons() {
+    this.resetButtons()
+    if(this.props.language === 'javascript') {
+      this.setState( { javascript: 'alive'} )
+    } else if(this.props.language === 'ruby') {
+      this.setState( { ruby: 'alive'} )
+    } else if(this.props.language === 'javascript') {
+      this.setState( { python: 'alive'} )
+    }
+  }
+
+  resetButtons() {
+    this.setState({
+      javascript: 'dead',
+      ruby: 'dead',
+      python: 'dead',
+    })
+  }
+
+  handleClick(language) {
+    this.props.selectLanguage(language);
+    this.setButtons();
+  }
+
+  render() {
+
+    return (
+      <div id="language-selector">
+        <div id="buttons" align="center">
+          <button id={this.state.javascript} onClick={() => this.handleClick('javascript')}>
+            Javascript
+          </button>
+          <button id={this.state.ruby} onClick={() => this.handleClick('ruby')}>
+            Ruby
+          </button>
+          <button id={this.state.python} onClick={() => this.handleClick('python')}>
+            Python
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  }
 
 }
+
+export default LanguageSelector;
